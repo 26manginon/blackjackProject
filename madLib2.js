4d4e8c24406007face560b562cf2d3eda80f6b2f
Mangino <26ManginoN@badinhs.org>
2025-12-18 14:08:44 -0500

    .

---
+++ madLib2.js
@@ -0,0 +1,34 @@
+let adjective1 = prompt("Enter a adjective");
+let noun1 = prompt("Enter a noun");
+let adjective2 = prompt("Enter a adjective");
+let place = prompt("Enter a place");
+let adjective3 = prompt("Enter a adjective");
+let adjective4 = prompt("Enter a adjective");
+let vehicle = prompt("Enter a vehicle");
+let adjective5 = prompt("Enter a adjective");
+let adjective6 = prompt("Enter a adjective");
+let pluralNoun = prompt("Enter a plural noun");
+let adjective7 = prompt("Enter adjective");
+let pluralNoun2 = prompt("Enter a plural noun");
+let pluralNoun3 = prompt("Enter a plural noun");
+let adjective8 = prompt("Enter a adjective");
+let noun2 = prompt("Enter a noun");
+let verb = prompt("Enter a verb");
+let adjective9 = prompt("Enter a adjective");
+let verb2 = prompt("Enter a verb");
+let pluralNoun4 = prompt("Enter a plural noun");
+let typeOfJob = prompt("Enter a type of job");
+let adjective10 = prompt("Enter a adjective");
+let verb3 = prompt("Enter a verb");
+let adjective11 = prompt("Enter a adjective");
+
+console.log(
+    "Star Wars is a" + "" + adjective1 + "" + noun1 + "" + "of" + "" + adjective2 + "" + "versus evil" + 
+     "" + "in a " + "" + place + "" + "far far away. There are" + "" + adjective3 + "" + "battles between" + 
+     "" + adjective4 + "" + vehicle + "" + "in" + "" + adjective5 + "" + "space and" + "" + adjective6 + "" + 
+     "duels with" + "" + pluralNoun + "" + "called" + "" + adjective7 + "" + "sabers." + "" + pluralNoun2 + "" + 
+     "called droids are helpers and" + "" + pluralNoun3 + "" + "to the heros. A" + "" + adjective8 + "" + 
+     "power called The" + "" + noun2 + "" + verb + "s" + "" + "people to do" + adjective9 + "" + 
+     "things like" + "" + verb2 + "" + typeOfJob + "" + "use The Force for the" + "" + adjective10 + "" + 
+     "side and the Sith" + "" +verb3 + "" + "it for the" + "" + adjective11 + "" + "side."
+)
---
+++ madLibs.js
@@ -0,0 +1,15 @@
+let adjective = prompt("Enter a adjective");
+let noun = prompt("Enter a noun");
+let verb =prompt("Enter a verb");
+let adjective2 =prompt("Enter a adjective");
+let place= prompt("Enter a place");
+let animal = prompt("Enter an animal");
+let verb2 = prompt("Enter a verb");
+let food = prompt("Enter a food");
+
+console.log(
+    " Today was a " + "" +  adjective + "" + " day at school. When I walked into my classroom, I saw a"  + "" +  noun + 
+    "" +  verb + "" + " on my desk! My teach looked" + "" +  adjective2 + "" + "and told us we were going on a" + 
+    "trip to" + "" +  place + "" + ". On the way, a" + "" +  animal + "" + "suddenly" + "" +  verb + ""+ "in front of the bus!"
+    + "At the end of the day, we celebrated by eating" + "" +  food + "" + "."
+);
