93f1c4ba9b79f7c3e4c7bcfbac4e8e7e007240ce
Mangino <26ManginoN@badinhs.org>
2025-12-18 11:15:56 -0500

    fixed repository

---
+++ blackJack.js
@@ -0,0 +1,13 @@
+let playerTotal = 0 
+let dealerTotal = 0 
+
+let playerHit = true;
+
+
+function deckMaker() {
+    let cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let cardSuits = ["Diamonds", "Hearts", "Spades", "Clubs"];
+  
+    for (let suitCounter = 0; suitCounter < 4; suitCounter + 1);
+    console.log(suits[suitCounter]);
+}
